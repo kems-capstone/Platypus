0070be0e7161998c4d40ea9bbcc12be3e98c6d48
Keith Nocera <knocera@gmail.com>
2019-05-01 16:41:45 -0400

    added spotify sdk to home page to test if it works on mobile

--- client/app.js
+++ client/app.js
@@ -5,6 +5,7 @@ export default class app extends Component {
     return (
       <div>
         <h1>Hey, this is the app</h1>
+        <iframe src="https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       </div>
     );
   }
