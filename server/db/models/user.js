619463b65b32e47fd40e94c5760263ce7b816685
Samiul Huque <Samiul.Huque@gmail.com>
2019-05-06 09:10:34 -0400

    added crypto and database now syncs

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
+const crypto = require('crypto');
 
 const User = db.define('user', {
   firstName: {
@@ -31,7 +32,7 @@ const User = db.define('user', {
     //   notEmpty: true
     // },
     get() {
-      return () => this.getDataValue('password')
+      return () => this.getDataValue('password');
     }
   },
   salt: {
@@ -42,50 +43,50 @@ const User = db.define('user', {
       notEmpty: true
     },
     get() {
-      return () => this.getDataValue('salt')
+      return () => this.getDataValue('salt');
     }
   },
   googleId: {
     type: Sequelize.STRING
   }
-})
+});
 
-module.exports = User
+module.exports = User;
 
 /**
  * instanceMethods
  */
 User.prototype.correctPassword = function(candidatePwd) {
-  return User.encryptPassword(candidatePwd, this.salt()) === this.password()
-}
+  return User.encryptPassword(candidatePwd, this.salt()) === this.password();
+};
 
 /**
  * classMethods
  */
 User.generateSalt = function() {
-  return crypto.randomBytes(16).toString('base64')
-}
+  return crypto.randomBytes(16).toString('base64');
+};
 
 User.encryptPassword = function(plainText, salt) {
   return crypto
     .createHash('RSA-SHA256')
     .update(plainText)
     .update(salt)
-    .digest('hex')
-}
+    .digest('hex');
+};
 
 /**
  * hooks
  */
 const setSaltAndPassword = user => {
   if (user.changed('password')) {
-    user.salt = User.generateSalt()
-    user.password = User.encryptPassword(user.password(), user.salt())
+    user.salt = User.generateSalt();
+    user.password = User.encryptPassword(user.password(), user.salt());
   }
-}
+};
 
-User.beforeCreate(setSaltAndPassword)
-User.beforeUpdate(setSaltAndPassword)
+User.beforeCreate(setSaltAndPassword);
+User.beforeUpdate(setSaltAndPassword);
 User.beforeBulkCreate(users => {
-  users.forEach(setSaltAndPassword)
-})
+  users.forEach(setSaltAndPassword);
+});
